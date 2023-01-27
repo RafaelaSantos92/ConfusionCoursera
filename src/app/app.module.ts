7cd11f98022663b37ea333f1b2c82af5dc2a7e55
Rafaela Santos <--santos.rafaela2991@gmail.com>
2023-01-27 10:35:12 -0300

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,13 +16,17 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
